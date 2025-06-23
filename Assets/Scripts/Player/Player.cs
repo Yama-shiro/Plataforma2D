cc0587b110c168162d2f737717925e5b13fb5df1
Yama-shiro <nicolasgyamashiro@gmail.com>
2025-06-23 09:35:15 -0300

    add jump p´rototype

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,11 +6,19 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
 
     public float speed;
 
+    public float forceJump = 2;
+
     private void Update()
+    {
+        HandleJump();
+        HandleMoviment();
+    }
+
+    private void HandleMoviment()
     {
         if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
@@ -23,5 +31,21 @@ public class Player : MonoBehaviour
             myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
         }
 
+
+        if(myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity += friction;
+        }
+        else if(myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+    }
+
+
+    private void HandleJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            myRigidbody.velocity = Vector2.up * forceJump;
     }
 }
