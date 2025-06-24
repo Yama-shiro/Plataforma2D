1d013fdcdc1450a1dbbb09e79646dfa5053d6c11
Yama-shiro <nicolasgyamashiro@gmail.com>
2025-06-24 09:08:42 -0300

    update

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,21 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
-    public Vector2 friction = new Vector2(.1f, 0);
 
+    [Header("Speed Setup")]
+    public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
     public float speedRun;
-
     public float forceJump = 2;
 
-    private float _currentSpeed;
 
+    [Header("Animation Setup")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = .7f;
+    public float animationDuration = .3f;
+    public Ease ease = Ease.OutBack;
+
+    private float _currentSpeed;
     private bool _isRunning = false;
 
     private void Update()
@@ -34,12 +41,10 @@ public class Player : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(-_currentSpeed, myRigidbody.velocity.y);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(_currentSpeed, myRigidbody.velocity.y);
         }
 
@@ -59,5 +64,16 @@ public class Player : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
             myRigidbody.velocity = Vector2.up * forceJump;
+                myRigidbody.transform.localScale = Vector2.one;
+
+        DOTween.Kill(myRigidbody.transform);
+
+            HandleScaleJump();
+    }
+
+    private void HandleScaleJump()
+    {
+        myRigidbody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myRigidbody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
 }
