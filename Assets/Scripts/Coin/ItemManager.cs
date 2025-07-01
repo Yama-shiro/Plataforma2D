d5de52778929fcd58698d01b1713a92add55b19c
Yama-shiro <nicolasgyamashiro@gmail.com>
2025-07-01 09:03:20 -0300

    scriptable objects

--- Assets/Scripts/Coin/ItemManager.cs
+++ Assets/Scripts/Coin/ItemManager.cs
@@ -8,7 +8,7 @@ using Ebac.Core.Singleton;
 public class ItemManager : Singleton<ItemManager>
 {
 
-    public int coins;
+    public SOInt coins;
     public TextMeshProUGUI uiTextCoins;
 
     public void Start()
@@ -18,19 +18,19 @@ public class ItemManager : Singleton<ItemManager>
 
     private void Reset()
     {
-        coins = 0;
+        coins.value = 0;
         UpdateUI();
     }
 
     public void AddCoins(int amount = 1)
     {
-        coins += amount;
+        coins.value += amount;
         UpdateUI();
     }
 
     private void UpdateUI()
     {
         //uiTextCoins.text = coins.ToString();
-        UIinGameManager.UpdateTextCoins(coins.ToString());
+        //UIinGameManager.UpdateTextCoins(coins.ToString());
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -20,6 +20,12 @@ public class Player : MonoBehaviour
     public float jumpScaleY = 1.5f;
     public float jumpScaleX = .7f;
     public float animationDuration = .3f;
+    public SOFloat soJumpScaleY;
+    public SOFloat soJumpScaleX;
+    public SOFloat soAnimationDuration;
+
+
+
     public Ease ease = Ease.OutBack;
 
     [Header("Animation Player")]
@@ -119,8 +125,8 @@ public class Player : MonoBehaviour
 
     private void HandleScaleJump()
     {
-        myRigidbody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
-        myRigidbody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myRigidbody.transform.DOScaleY(soJumpScaleY.value, soAnimationDuration.value).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myRigidbody.transform.DOScaleX(soJumpScaleX.value, soAnimationDuration.value).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
 
     public void DestroyMe()
