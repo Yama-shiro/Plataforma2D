96794dd9223e816abdd7583158d344c98215d37b
Yama-shiro <nicolasgyamashiro@gmail.com>
2025-07-01 08:43:34 -0300

    update ui

--- Assets/Scripts/Coin/ItemManager.cs
+++ Assets/Scripts/Coin/ItemManager.cs
@@ -30,6 +30,7 @@ public class ItemManager : Singleton<ItemManager>
 
     private void UpdateUI()
     {
-        uiTextCoins.text = coins.ToString();
+        //uiTextCoins.text = coins.ToString();
+        UIinGameManager.UpdateTextCoins(coins.ToString());
     }
 }
