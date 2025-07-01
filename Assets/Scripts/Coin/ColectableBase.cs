7520a589989c9b505f2978d83dc9fb997a3aaa5d
Yama-shiro <nicolasgyamashiro@gmail.com>
2025-07-01 10:54:58 -0300

    moeda

--- Assets/Scripts/Coin/ColectableBase.cs
+++ Assets/Scripts/Coin/ColectableBase.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class ColectableBase : MonoBehaviour
 {
     public string compareTag = "Player";
+    public ParticleSystem particleSystem;
+    public float timeToHide = 3;
+    public GameObject graphicItem;
+
+    private void Awake()
+    {
+        //if (particleSystem != null) particleSystem.transform.SetParent(null);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -17,10 +25,19 @@ public class ColectableBase : MonoBehaviour
 
     protected virtual void Collect()
     {
-        gameObject.SetActive(false);
+        if(graphicItem != null) graphicItem.SetActive(false);
+        Invoke("HideObject", timeToHide);
         OnCollect();
     }
 
-    protected virtual void OnCollect() { }
+    private void HideObjects()
+    {
+        gameObject.SetActive(false);
+    }
+
+    protected virtual void OnCollect() 
+    {
+        if(particleSystem != null) particleSystem.Play();
+    }
     
 }
